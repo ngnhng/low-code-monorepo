f3cfa555cbebcb78bd1cc8e5425d78de6356a099
nguyendhst <nguyendhst@proton.me>
2023-10-12 14:42:48 +0700

    hotfix: change flow call

--- apps/web/app/auth/login/page.tsx
+++ apps/web/app/auth/login/page.tsx
@@ -1,32 +1,13 @@
 import { redirect } from "next/navigation";
 
 export default function Page() {
-    const googleOAuthURL = "https://accounts.google.com/o/oauth2/v2/auth";
     const redirectUri = "http://localhost:3000/api/oauth/google/callback";
-
-    const scope = [
-        "https://www.googleapis.com/auth/userinfo.email",
-        "https://www.googleapis.com/auth/userinfo.profile",
-    ].join(" ");
-
-    const params = new URLSearchParams({
-        scope,
-        redirect_uri: redirectUri,
-        response_type: "code",
-        prompt: "select_account",
-        // eslint-disable-next-line turbo/no-undeclared-env-vars
-        client_id: process.env.GOOGLE_OAUTH_CLIENT_ID!,
-    }).toString();
-
-    console.log(params);
-
-    // redirect to google oauth
-    //redirect(`${googleOAuthURL}?${params}`);
+    const serverOAuthURL = "http://localhost:3000/api/oauth/google";
 
     return (
         <div>
             <h1>Google OAuth</h1>
-            <a href={`${googleOAuthURL}?${params}`}>Login with Google</a>
+            <a href={`${serverOAuthURL}?`}>Login with Google</a>
         </div>
     );
 }
