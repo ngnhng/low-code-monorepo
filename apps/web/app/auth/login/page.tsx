a84c9a185c19981ee76d7648a60ce1f723bbbb91
NgTrNghia1211 <luckilyislife@gmail.com>
2023-10-20 12:37:21 +0700

    little css login button

--- apps/web/app/auth/login/page.tsx
+++ apps/web/app/auth/login/page.tsx
@@ -1,13 +1,33 @@
 import { redirect } from "next/navigation";
+import { headers } from 'next/headers'; 
+import './style.css';
 
-export default function Page() {
+
+export default function LoginPage() {
     const redirectUri = "http://localhost:3000/api/oauth/google/callback";
     const serverOAuthURL = "http://localhost:3000/api/oauth/google";
 
     return (
-        <div>
-            <h1>Google OAuth</h1>
-            <a href={`${serverOAuthURL}?`}>Login with Google</a>
-        </div>
+			<div className="container">
+				<div className="form-container">
+					<h1>Login</h1>
+					<div className="form">
+						<p>Access feature by google login</p>
+
+						<a href={serverOAuthURL}>
+							<div className="g-login-button">
+								<div className="content-wrapper">
+									<div className="logo-wrapper">
+										<img src="https://developers.google.com/identity/images/g-logo.png" alt="" />
+									</div>
+									<span className="text-container">
+										Sign in with Google
+									</span>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
     );
 }
