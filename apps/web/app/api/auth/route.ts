e4f95b44d00e5463f3aa3f0369764c35160b2dad
nguyendhst <nguyendhst@proton.me>
2023-10-26 21:53:13 +0700

    refactor auth function

--- apps/web/app/api/auth/route.ts
+++ apps/web/app/api/auth/route.ts
@@ -1,40 +1,41 @@
 import { EnvVariable, getEnv } from "../../../utils/getEnv";
-import axios from "axios";
 
 type TokenResponse = [string, string];
 
 export async function GET(request: Request) {
-    const serverOAuthUrl = getEnv(EnvVariable.LOGIN_REQUEST);
-    const serverCallbackUrl = getEnv(EnvVariable.LOGIN_CALLBACK);
-    const successUrl = "http://localhost:3001/auth/login?success=1";
+   const serverOAuthUrl = getEnv(EnvVariable.LOGIN_REQUEST);
+   const serverCallbackUrl = getEnv(EnvVariable.LOGIN_CALLBACK);
+   const successUrl = "/auth/login?success=1";
+   const failureUrl = "/auth/login?success=0";
 
-    // get query param ?success=1
-    const incomingUrl = new URL(request.url ?? "");
-    const params = incomingUrl.searchParams;
+   // get query param ?success=1
+   const incomingUrl = new URL(request.url ?? "");
+   const params = incomingUrl.searchParams;
 
-    if (params.get("code")) {
-        // split from the first ?
-        const [_, query] = incomingUrl.search.split("?");
+   if (params.get("code")) {
+      // split from the first ?
+      const [_, query] = incomingUrl.search.split("?");
 
-        // append the query to the callback url
-        const callbackUrl = new URL(serverCallbackUrl);
-        callbackUrl.search = query;
+      // append the query to the callback url
+      const callbackUrl = new URL(serverCallbackUrl);
+      callbackUrl.search = query;
 
-        console.log(callbackUrl.toString());
+      console.log(callbackUrl.toString());
 
-        // GET the callback url
-        const response = await axios
-            .get(callbackUrl.toString())
-            .catch((err) => {
-                console.log(err);
-            });
+      // GET the callback url
+      const response = await fetch(callbackUrl.toString(), {
+         method: "GET",
+      }).catch((err) => {
+         console.error(err);
+         return Response.redirect(failureUrl, 302);
+      });
 
-        console.log(response);
+      console.log(response);
 
-        // redirect to the success page
-        return Response.redirect(successUrl, 302);
-    }
+      // redirect to the success page
+      return Response.redirect(successUrl, 302);
+   }
 
-    // redirect caller to the server oauth url
-    return Response.redirect(serverOAuthUrl, 302);
+   // redirect caller to the server oauth url
+   return Response.redirect(serverOAuthUrl, 302);
 }
