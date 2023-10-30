07a53b27f1e138a25f146f7a3a66f0531541f8a0
NgTrNghia1211 <luckilyislife@gmail.com>
2023-10-30 20:38:23 +0700

    init workflow branch

--- apps/web/app/[project-id]/workflow/page.tsx
+++ apps/web/app/[project-id]/workflow/page.tsx
@@ -1,3 +1,79 @@
+'use client';
+
+import { useState, useCallback } from 'react';
+
+import 'reactflow/dist/style.css';
+
+import ReactFlow, {
+  addEdge,
+  FitViewOptions,
+	DefaultEdgeOptions,
+  applyNodeChanges,
+  applyEdgeChanges,
+  Node,
+  Edge,
+  OnNodesChange,
+  OnEdgesChange,
+  OnConnect,
+} from 'reactflow';
+
+// import CustomNode from './CustomNode';
+
+const initialNodes: Node[] = [
+  { id: '1', data: { label: 'Node 1' }, position: { x: 5, y: 5 } },
+  { id: '2', data: { label: 'Node 2' }, position: { x: 5, y: 100 } },
+];
+
+const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2' }];
+
+const fitViewOptions: FitViewOptions = {
+  padding: 0.2,
+};
+
+const defaultEdgeOptions: DefaultEdgeOptions = {
+  animated: true,
+};
+
+// const nodeTypes: NodeTypes = {
+//   custom: CustomNode,
+// };
+
+function Flow() {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange: OnNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange: OnEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+  const onConnect: OnConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView
+      fitViewOptions={fitViewOptions}
+      defaultEdgeOptions={defaultEdgeOptions}
+      // nodeTypes={nodeTypes}
+    />
+  );
+}
+
 export default function Page() {
-    return <></>
+  return (
+    <>
+			<Flow />
+    </>
+  )
 }
