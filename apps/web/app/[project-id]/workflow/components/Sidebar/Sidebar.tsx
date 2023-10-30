057f1ccc6c811776921b7c10bb318b4886298cc7
NgTrNghia1211 <luckilyislife@gmail.com>
2023-10-30 23:36:47 +0700

    default drag and drop

---
+++ apps/web/app/[project-id]/workflow/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+// import { Node } from 'reactflow';
+
+const Sidebar = () => {
+  const onDragStart = (event: React.DragEvent, nodeType) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  }
+
+  return (
+    <aside>
+      <div className="description">You can drag these nodes to the pane on the right.</div>
+      <div className="dndnode input" onDragStart={(event) => onDragStart(event, 'input')} draggable>
+        Input Node
+      </div>
+      <div className="dndnode" onDragStart={(event) => onDragStart(event, 'default')} draggable>
+        Default Node
+      </div>
+      <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'output')} draggable>
+        Output Node
+      </div>
+    </aside>
+  );
+}
+
+export default Sidebar
--- apps/web/app/[project-id]/workflow/page.tsx
+++ apps/web/app/[project-id]/workflow/page.tsx
@@ -1,79 +1,167 @@
 'use client';
 
-import { useState, useCallback } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 
 import 'reactflow/dist/style.css';
 
+import './components/Sidebar/style.css';
+
 import ReactFlow, {
+  ReactFlowProvider,
+  useNodesState,
+  useEdgesState,
+  Controls,
   addEdge,
-  FitViewOptions,
-	DefaultEdgeOptions,
-  applyNodeChanges,
-  applyEdgeChanges,
+  // FitViewOptions,
+	// DefaultEdgeOptions,
+  // applyNodeChanges,
+  // applyEdgeChanges,
   Node,
   Edge,
-  OnNodesChange,
-  OnEdgesChange,
+  // OnNodesChange,
+  // OnEdgesChange,
   OnConnect,
+  ReactFlowInstance,
 } from 'reactflow';
+
+import Sidebar from './components/Sidebar/Sidebar';
 
 // import CustomNode from './CustomNode';
 
 const initialNodes: Node[] = [
-  { id: '1', data: { label: 'Node 1' }, position: { x: 5, y: 5 } },
-  { id: '2', data: { label: 'Node 2' }, position: { x: 5, y: 100 } },
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'input node' },
+    position: { x: 250, y: 5 },
+  },
 ];
 
-const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2' }];
+let id = 0;
+const getId = () => `dndnode_${id++}`;
 
-const fitViewOptions: FitViewOptions = {
-  padding: 0.2,
-};
+const DnDFlow = () => {
+  const reactFlowWrapper = useRef<HTMLParagraphElement>(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState<Node[]>(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState<Edge[]>([]);
+  const [reactFlowInstance, setReactFlowInstance] = useState<ReactFlowInstance | null>(null);
 
-const defaultEdgeOptions: DefaultEdgeOptions = {
-  animated: true,
-};
-
-// const nodeTypes: NodeTypes = {
-//   custom: CustomNode,
-// };
-
-function Flow() {
-  const [nodes, setNodes] = useState<Node[]>(initialNodes);
-  const [edges, setEdges] = useState<Edge[]>(initialEdges);
-
-  const onNodesChange: OnNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-    [setNodes]
-  );
-  const onEdgesChange: OnEdgesChange = useCallback(
-    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    [setEdges]
-  );
   const onConnect: OnConnect = useCallback(
     (connection) => setEdges((eds) => addEdge(connection, eds)),
     [setEdges]
   );
 
+  const onDragOver = useCallback((event: React.DragEvent) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
+
+  const onDrop = useCallback((event: React.DragEvent) => {
+    event.preventDefault();
+
+    if (reactFlowWrapper) {
+      const reactFlowBounds = reactFlowWrapper.current?.getBoundingClientRect();
+      const type = event.dataTransfer.getData('application/reactflow');
+
+      if (typeof type === 'undefined' || !type) {
+        return;
+      }
+
+      if (reactFlowInstance && reactFlowBounds) {
+        const position = reactFlowInstance.project({
+          x: event.clientX - reactFlowBounds.left,
+          y: event.clientY - reactFlowBounds.top,
+        });
+
+        const newNode: Node = {
+          id: getId(),
+          type,
+          position,
+          data: { label: `${type} node` },
+        };
+
+        setNodes((nds) => nds.concat(newNode));
+
+      }
+    } else {
+      return;
+    }
+  }, [reactFlowInstance])
+
   return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      fitView
-      fitViewOptions={fitViewOptions}
-      defaultEdgeOptions={defaultEdgeOptions}
-      // nodeTypes={nodeTypes}
-    />
+    <div className="dndflow">
+      <ReactFlowProvider>
+        <Sidebar />
+        <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onInit={setReactFlowInstance}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            fitView
+          >
+            <Controls position='bottom-left'/>
+          </ReactFlow>
+        </div>
+      </ReactFlowProvider>
+    </div>
   );
 }
+
+// const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2' }];
+
+// const fitViewOptions: FitViewOptions = {
+//   padding: 0.2,
+// };
+
+// const defaultEdgeOptions: DefaultEdgeOptions = {
+//   animated: true,
+// };
+
+// const nodeTypes: NodeTypes = {
+//   custom: CustomNode,
+// };
+
+// function Flow() {
+//   const [nodes, setNodes] = useState<Node[]>(initialNodes);
+//   const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+//   const onNodesChange: OnNodesChange = useCallback(
+//     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+//     [setNodes]
+//   );
+//   const onEdgesChange: OnEdgesChange = useCallback(
+//     (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+//     [setEdges]
+//   );
+//   const onConnect: OnConnect = useCallback(
+//     (connection) => setEdges((eds) => addEdge(connection, eds)),
+//     [setEdges]
+//   );
+
+//   return (
+//     <ReactFlow
+//       nodes={nodes}
+//       edges={edges}
+//       onNodesChange={onNodesChange}
+//       onEdgesChange={onEdgesChange}
+//       onConnect={onConnect}
+//       fitView
+//       fitViewOptions={fitViewOptions}
+//       defaultEdgeOptions={defaultEdgeOptions}
+//       // nodeTypes={nodeTypes}
+//     />
+//   );
+// }
 
 export default function Page() {
   return (
     <>
-			<Flow />
+			<DnDFlow />
     </>
   )
 }
