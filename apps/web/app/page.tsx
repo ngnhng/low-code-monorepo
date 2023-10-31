4a91998998c567c8de529e66e47c76acc71737d7
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2023-10-31 21:27:21 +0700

    Added original template for landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,35 @@
+import "./styles.css";
+
+import Link from "next/link";
+
 export default function Page() {
-    return (
-        <div>
-            Placeholder for Projects Management Page
-        </div>
-    );
+   return (
+      <>
+         <div className="navBar">
+            <div className="left">
+               <Link href="./">
+                  <div className="home"></div>
+               </Link>
+               <Link href="./">
+                  <div className="navItem">Projects</div>
+               </Link>
+               <Link href="./">
+                  <div className="navItem">Placeholder #1</div>
+               </Link>
+               <Link href="./">
+                  <div className="navItem">Placeholder #2</div>
+               </Link>
+               <Link href="./">
+                  <div className="navItem">Placeholder #3</div>
+               </Link>
+            </div>
+
+            <div className="right">
+               <Link href="./auth/login">
+                  <div className="navItem">Sign in</div>
+               </Link>
+            </div>
+         </div>
+      </>
+   );
 }
