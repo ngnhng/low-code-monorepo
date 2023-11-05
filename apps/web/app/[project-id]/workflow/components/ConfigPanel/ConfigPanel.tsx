dbc0e96878bd22e78e44bf63062760d2c9a24b09
nguyendhst <nguyendhst@proton.me>
2023-11-05 14:51:54 +0700

    revamp ui

--- apps/web/app/[project-id]/workflow/components/ConfigPanel/ConfigPanel.tsx
+++ apps/web/app/[project-id]/workflow/components/ConfigPanel/ConfigPanel.tsx
@@ -1,29 +1,111 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const ConfigPanel = ({id, label, setNodes}) => {
-  console.log("props: ", id);
-  console.log("label: ", label);
+const FloatingPanel = ({ children, top }) => {
+   return (
+      <div
+         style={{
+            position: "fixed",
+            top: top,
+            right: "20px",
+            backgroundColor: "#fff",
+            padding: "10px",
+            borderRadius: "5px",
+            boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
+            height: "fit-content",
+            transform: "translateY(-50%)",
+            maxWidth: "120px",
+         }}
+      >
+         {children}
+      </div>
+   );
+};
 
-  const [nodeName, setNodeName] = useState('Node 1');
+const ConfigPanel = ({ id, label, setNodes, onSave, onRestore }) => {
+   const [nodeName, setNodeName] = useState("Node 1");
 
-  return (
-    <div className='configNodePanel'>
-      <label>label:</label>
-      <input value={`test`} onChange={() => {}} />
+   return (
+      <>
+         <FloatingPanel top="50%">
+            <label>Label:</label>
+            <input
+               value={`test`}
+               onChange={() => {}}
+               style={{
+                  padding: "5px",
+                  borderRadius: "3px",
+                  border: "1px solid #ccc",
+                  marginBottom: "10px",
+                  width: "100%",
+               }}
+            />
 
-      <label className="configNodePanel_bg">background:</label>
-      <input value={`temp`} onChange={() => {}} />
+            <label className="configNodePanel_bg">Background:</label>
+            <input
+               value={`temp`}
+               onChange={() => {}}
+               style={{
+                  padding: "5px",
+                  borderRadius: "3px",
+                  border: "1px solid #ccc",
+                  marginBottom: "10px",
+                  width: "100%",
+               }}
+            />
 
-      <div className="configNodePanel_check">
-          <label>hidden:</label>
-          <input
-            type="checkbox"
-            checked={true}
-            onChange={(evt) => {}}
-          />
-        </div>
-    </div>
-  )
-}
+            <div className="configNodePanel_check">
+               <label>Hidden?:</label>
+               <input
+                  type="checkbox"
+                  checked={true}
+                  onChange={(evt) => {}}
+                  style={{
+                     marginRight: "10px",
+                  }}
+               />
+            </div>
+         </FloatingPanel>
 
-export default ConfigPanel
+         <FloatingPanel top="70%">
+			<div
+				style={{
+					margin: "10px",
+				}}
+			>
+            <button
+               onClick={onSave}
+               style={{
+                  borderRadius: "5px",
+                  border: "none",
+                  backgroundColor: "#008CBA",
+                  color: "white",
+                  cursor: "pointer",
+				  width: "100%",
+				  height: "40px",
+				  margin: "5px",
+               }}
+            >
+               Save
+            </button>
+            <button
+               onClick={onRestore}
+               style={{
+                  borderRadius: "5px",
+                  border: "none",
+                  backgroundColor: "#f44336",
+                  color: "white",
+                  cursor: "pointer",
+				  width: "100%",
+				  height: "40px",
+				  margin: "5px",
+               }}
+            >
+               Restore
+            </button>
+			</div>
+         </FloatingPanel>
+      </>
+   );
+};
+
+export default ConfigPanel;
--- apps/web/app/[project-id]/workflow/components/Sidebar/Sidebar.tsx
+++ apps/web/app/[project-id]/workflow/components/Sidebar/Sidebar.tsx
@@ -1,25 +1,62 @@
-import React from 'react'
-// import { Node } from 'reactflow';
-import { CustomNodeType } from '../../utils';
+import React, { useState } from "react";
+import { CustomNodeType } from "../../utils";
 
 const Sidebar = () => {
-  const onDragStart = (event: React.DragEvent, nodeType) => {
-    event.dataTransfer.setData('application/reactflow', nodeType);
-    event.dataTransfer.effectAllowed = 'move';
-  }
+   const onDragStart = (event: React.DragEvent, nodeType) => {
+      event.dataTransfer.setData("application/reactflow", nodeType);
+      event.dataTransfer.effectAllowed = "move";
+   };
 
-  return (
-    <aside>
-      <div className="description">You can drag these nodes to the pane on the right.</div>
-      {
-        CustomNodeType.map((type, index ) => (
-          <div key={index} className="dndnode" onDragStart={(event) => onDragStart(event, type)} draggable>
-            {`${type.toUpperCase()} Node`}
-          </div>
-        ))
-      }
-    </aside>
-  );
-}
+   const nodeGroups = ["Group 1", "Group 2", "Group 3"];
+   const [expandedGroups, setExpandedGroups] = useState<string[]>([]);
 
-export default Sidebar
+   const toggleGroup = (group: string) => {
+      setExpandedGroups((prev) =>
+         prev.includes(group)
+            ? prev.filter((g) => g !== group)
+            : [...prev, group]
+      );
+   };
+
+   return (
+      <aside
+	  	style={{
+			backgroundColor: "var(--puck-color-rose-9)",
+		}}
+	  >
+         <div className="description">
+            You can drag these nodes to the pane on the right.
+         </div>
+         {nodeGroups.map((group, index) => (
+            <div key={index}>
+               <div
+                  style={{
+                     backgroundColor: expandedGroups.includes(group)
+                        ? "var(--puck-color-rose-6)"
+                        : "var(--puck-color-rose-7)",
+                     color: "white",
+                     padding: "5px",
+                     cursor: "pointer",
+                  }}
+                  onClick={() => toggleGroup(group)}
+               >
+                  {group}
+               </div>
+               {expandedGroups.includes(group) &&
+                  CustomNodeType.map((type, index) => (
+                     <div
+                        key={index}
+                        className="dndnode"
+                        onDragStart={(event) => onDragStart(event, type)}
+                        draggable
+                     >
+                        {`${type.toUpperCase()} Node`}
+                     </div>
+                  ))}
+            </div>
+         ))}
+      </aside>
+   );
+};
+
+export default Sidebar;
