aafc70038d89791b652d026c2e4fe209f4e88159
nguyendhst <nguyendhst@proton.me>
2024-02-27 14:37:46 +0700

    fix(ui): add missing use client

--- packages/ui/src/form/form.tsx
+++ packages/ui/src/form/form.tsx
@@ -1,5 +1,9 @@
 "use client";
+"use client";
 
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { Slot } from "@radix-ui/react-slot";
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { Slot } from "@radix-ui/react-slot";
@@ -13,8 +17,9 @@ import {
 } from "react-hook-form";
 
 import { cn } from "../lib/utils";
-import { Label } from "../label/label";
+import { Label } from "../label";
 
+const Form = FormProvider;
 const Form = FormProvider;
 
 type FormFieldContextValue<
@@ -136,7 +141,7 @@ const FormDescription = React.forwardRef<
     <p
       ref={ref}
       id={formDescriptionId}
-      className={cn("text-sm text-muted-foreground", className)}
+      className={cn("text-[0.8rem] text-muted-foreground", className)}
       {...props}
     />
   );
@@ -158,7 +163,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-sm font-medium text-destructive", className)}
+      className={cn("text-[0.8rem] font-medium text-destructive", className)}
       {...props}
     >
       {body}
