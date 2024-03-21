1486d83555239da51079319cf0c48feefd957f0a
NgTrNghia1211 <nghia.nguyenmysterios@hcmut.edu.vn>
2024-03-21 21:33:17 +0700

    feat: rework query-builder-ui

--- apps/web/app/[projectId]/data/[tableId]/_components/query-builder-list.tsx
+++ apps/web/app/[projectId]/data/[tableId]/_components/query-builder-list.tsx
@@ -6,8 +6,7 @@ import {
   Field,
   QueryBuilder,
   RuleGroupType,
-  RuleType,
-  defaultOperators,
+  formatQuery,
 } from 'react-querybuilder';
 import 'react-querybuilder/dist/query-builder.css';
 
@@ -21,21 +20,30 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
   Input,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from '@repo/ui';
 import { toast } from 'sonner';
 import { useMobxStore } from 'lib/mobx/store-provider';
 import useSWR from 'swr';
+import { ColumnDef } from 'types/table-data';
 
 const initialQuery: RuleGroupType = { combinator: 'and', rules: [] };
 
 interface QueryBuilderListProps {
-  columns: any;
+  columns: ColumnDef[];
   tableId: string;
 }
 
 // eslint-disable-next-line no-unused-vars
-const QueryBuilderList = ({ tableId }: QueryBuilderListProps) => {
+const QueryBuilderList = ({ tableId, columns }: QueryBuilderListProps) => {
   const [query, setQuery] = useState(initialQuery);
+  const [groupby, setGroupBy] = useState<string>();
+  const [sortby, setSortBy] = useState<string>();
 
   const {
     projectData: { currentProjectId },
@@ -64,12 +72,15 @@ const QueryBuilderList = ({ tableId }: QueryBuilderListProps) => {
   };
 
   const onQuery = () => {
+    const postValues = { ...query, groupby: groupby, sortby: sortby };
+
     toast.success('Column has been created.', {
       description: (
         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
           <code className="text-white">
-            {JSON.stringify(query, undefined, 2)}
+            {JSON.stringify(postValues, undefined, 2)}
           </code>
+          {/* <p className="font-bold text-white">Group by: {groupby}</p> */}
         </pre>
       ),
     });
@@ -81,26 +92,73 @@ const QueryBuilderList = ({ tableId }: QueryBuilderListProps) => {
     <div>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant={'default'}>Query Builder</Button>
+          <Button variant={'primary'}>Query Builder</Button>
         </DropdownMenuTrigger>
 
-        <DropdownMenuContent>
-          <DropdownMenuLabel>Action Filter</DropdownMenuLabel>
+        <DropdownMenuContent className="min-w-[750px] max-w-full">
+          <DropdownMenuLabel>Query Fields</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuGroup>
             <QueryBuilder
               fields={queryFields}
               query={query}
               onQueryChange={(q) => setQuery(q)}
+              autoSelectField={false}
+              translations={{
+                fields: {
+                  placeholderLabel: 'Field format: table - column...',
+                },
+              }}
             />
-            <Button className="my-4" variant={'secondary'} onClick={onQuery}>
-              Query
-            </Button>
+            <div className="flex items-center justify-between">
+              <DropdownMenuItem className="mt-4" onSelect={onSelect}>
+                <span className="font-bold mr-4">Group by</span>
+                <Select onValueChange={(value) => setGroupBy(value)}>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Select ..." />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      {columns.map((col) => (
+                        <SelectItem key={col.id} value={col.id}>
+                          {col.label}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem className="mt-4" onSelect={onSelect}>
+                <span className="font-bold mr-4">Sort By</span>
+                <Select onValueChange={(value) => setSortBy(value)}>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Select ..." />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="asc">ASC</SelectItem>
+                      <SelectItem value="desc">DESC</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </DropdownMenuItem>
+
+              <div />
+            </div>
             <DropdownMenuItem className="" onSelect={onSelect}>
-              Filter By Values
-              <Input type="email" placeholder="Find ..." />
+              <span>Preview SQL</span>
+              <Input
+                disabled
+                value={formatQuery(query, 'sql')}
+                className="font-bold text-blue-700"
+              />
             </DropdownMenuItem>
           </DropdownMenuGroup>
+
+          <Button className="my-4" variant={'secondary'} onClick={onQuery}>
+            Query
+          </Button>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
