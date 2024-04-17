28cbd55acd47edad3e9732acc7a4f583a42ec3e0
NgTrNghia1211 <nghia.nguyenmysterios@hcmut.edu.vn>
2024-04-17 21:58:19 +0700

    feat: add logic mock data for sheetData

--- apps/web/app/api/data/sheet/route.ts
+++ apps/web/app/api/data/sheet/route.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
+import { ColumnDef, RowDef } from "types/table-data";
 
 export async function POST(request: NextRequest) {
   const bearerToken = getBearerToken(
@@ -36,7 +37,10 @@ export async function POST(request: NextRequest) {
     }
   );
 
-  console.log("Google Sheet:", sheetData.data);
+  console.log(
+    "Google Sheet:",
+    temporaryFormatGoogleSheetData(sheetData.data.values, spreadsheetsId)
+  );
 
   return NextResponse.json(sheetData.data.values);
 }
@@ -53,4 +57,61 @@ function getBearerToken(authorizationHeader: string): string | undefined {
   // Split the header on space and return the second element (token)
   const parts = authorizationHeader.split(" ");
   return parts[1];
+}
+
+function temporaryFormatGoogleSheetData(
+  sheetData: string[][],
+  spreadsheetsId: string
+) {
+  if (sheetData.length === 0 || !sheetData[0]) {
+    return;
+  }
+
+  // Create columns
+  const numberOfColumns = sheetData[0].length;
+  const tempColumns: ColumnDef[] = [];
+
+  for (let i = 0; i < numberOfColumns; i++) {
+    tempColumns.push({
+      id: `col-${i}`,
+      label: `Column ${i}`,
+      type: "text",
+      isActive: true,
+      isPrimaryKey: true,
+      isForeignKey: false,
+      foreignKeyId: "",
+    });
+  }
+
+  // Create rows
+  const rows: RowDef[] = [];
+  // eslint-disable-next-line unicorn/no-for-loop
+  for (let i = 0; i < sheetData.length; i++) {
+    if (!sheetData[i]) {
+      continue;
+    }
+
+    rows.push({
+      id: i + 1,
+      ...combineArraysToObject(tempColumns, sheetData[i]!),
+    });
+  }
+
+  const response = {
+    columns: tempColumns,
+    rows: rows,
+  };
+
+  return response;
+}
+
+function combineArraysToObject(
+  ids: ColumnDef[],
+  values: string[]
+): { [key: string]: string } {
+  if (ids.length !== values.length) {
+    throw new Error("Arrays must have the same length");
+  }
+
+  return Object.fromEntries(ids.map((id, index) => [id.id, values[index]!]));
 }
