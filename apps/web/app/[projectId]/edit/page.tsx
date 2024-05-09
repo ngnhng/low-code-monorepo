db08e033e562732790d846f54f22b5a73fa0c6cd
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2024-05-09 15:09:26 +0700

    feat: route selector page / route change / UI API refactor

--- apps/web/app/[projectId]/edit/page.tsx
+++ apps/web/app/[projectId]/edit/page.tsx
@@ -1,151 +1,67 @@
+/* eslint-disable unicorn/no-nested-ternary */
 "use client";
 
-import "./style.css";
-
-import type { Data } from "@measured/puck";
-import { Render, Puck } from "@measured/puck";
-import "@measured/puck/puck.css";
-
-import { useState } from "react";
-import config from "./_config";
-import { Switch, Label, Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@repo/ui";
-import axios from "axios";
+import Title from "components/title/title";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, Button } from "@repo/ui";
 import useSWR from "swr";
+import { Trash2 } from "lucide-react";
+import Link from "next/link";
 
-export default function Page() {
-    const [route, setRoute] = useState<string>("/");
-    const componentKey = Buffer.from(Object.keys(config.components).join("-")).toString("base64");
-    const key = `puck-demo:${componentKey}:${route}`;
+export default function Page({ params }: { params: { projectId: string } }) {
+    const { data, isLoading, error } = useSWR("/api/ui", async (url) => {
+        const res = await fetch(url);
 
+        if (!res.ok) {
+            const mes = await res.json();
+            throw new Error(mes.error);
+        }
 
-    const { data: dataFromAPI, isLoading } = useSWR("/api/ui", async (url) => {
-        const res = await axios.get(url);
-        console.log(res.data);
-        return res.data;
+        return await res.json();
     });
 
-    const [isEdit, setIsEdit] = useState<boolean>(true);
-
-    const handleToggle = () => {
-        setIsEdit(!isEdit);
-    };
-
-    const editSwitch = <SwitchGroup handleToggle={handleToggle} isOn={isEdit} disabled={isLoading} />;
-    const selectRoute = (
-        <div className="flex gap-2.5 items-center">
-            <Label>Select Route</Label>
-            <Select
-                onValueChange={(value) => {
-                    setRoute(value);
-                    console.log(dataFromAPI[value]);
-                }}
-                defaultValue={route}
-            >
-                <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Select a route" />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectGroup>
-                        {Object.keys(dataFromAPI ?? {}).map((route) => (
-                            <SelectItem value={route} key={route}>
-                                {route}
-                            </SelectItem>
-                        ))}
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
-        </div>
-    );
-
-    const toolbarItems = [
-        { key: "edit", icon: "/edit.png", component: editSwitch },
-        { key: "route", icon: "/edit.png", component: selectRoute },
-    ];
+    // const createNewPage = async () => {
+    //     // Do Something
+    // }
 
     return (
-        <div className="flex-1 flex flex-col gap-2.5">
-            <Toolbar items={toolbarItems} />
-            {isLoading ? (
-                ""
-            ) : (
-                <div
-                    className={`${isEdit ? "flex" : "border-2 border-slate-300 rounded-md"} flex-1 box-border puckContainer ${
-                        isEdit ? "overflow-hidden" : "overflow-auto"
-                    }`}
-                >
-                    {isEdit ? (
-                        // https://puckeditor.com/docs/extending-puck/custom-interfaces
-                        <Puck
-                            config={config}
-                            data={dataFromAPI[route]}
-                            headerPath={route}
-                            // onChange={setData}
-                            onPublish={async (data: Data) => {
-                                localStorage.setItem(key, JSON.stringify(data));
-                            }}
-                            key={route}
-                        >
-                            <div className="gap-2.5 flex-1 overflow-hidden flex">
-                                <div className="bg-slate-100 rounded-md border-2 border-slate-300 w-52 overflow-auto">
-                                    <Puck.Fields />
-                                </div>
-                                <div className="h-full flex-1 border-2 border-slate-300 rounded-md p-2.5 overflow-auto">
-                                    <Puck.Preview />
-                                </div>
-                                <div className="bg-slate-100 p-2.5 rounded-md border-2 border-slate-300 w-52 overflow-auto">
-                                    <Puck.Components />
-                                </div>
-                            </div>
-                        </Puck>
-                    ) : (
-                        <div className="flex-1 overflow-auto">
-                            <div className="w-full h-full overflow-auto">
-                                <Render
-                                    config={config}
-                                    data={
-                                        dataFromAPI[route] ?? {
-                                            content: [],
-                                            root: { props: { title: "Test" } },
-                                            zones: {},
-                                        }
-                                    }
-                                />
-                            </div>
-                        </div>
-                    )}
-                </div>
-            )}
+        <div className="w-full h-full rounded-md border-2 border-slate-300 p-[30px] flex flex-col gap-5">
+            <Title name="UI Builder" description="Think fast!" />
+            <div className="w-full h-[1px] bg-slate-300"></div>
+            <Button className="w-full">Create new page</Button>
+            <div className="w-full rounded-md border-2 border-slate-300 p-5">
+                {isLoading ? (
+                    "Loading..."
+                ) : error ? (
+                    error.message
+                ) : (
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px]">Route</TableHead>
+                                <TableHead></TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {data.map((routeData) => {
+                                return (
+                                    <TableRow className="hover:bg-slate-100 p-0" key={routeData.id}>
+                                        <TableCell className="font-medium w-full relative">
+                                            <Link href={`/${params.projectId}/edit/${routeData.id}`} className="w-full hover:cursor-pointer">
+                                                <div className="w-full p-5">{routeData.route}</div>
+                                            </Link>
+                                        </TableCell>
+                                        <TableCell className="text-right">
+                                            <button className="p-2.5 rounded-md hover:text-red-500">
+                                                <Trash2 />
+                                            </button>
+                                        </TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                )}
+            </div>
         </div>
-    );
-}
-
-interface ToolbarProps {
-    items: ToolbarItemProps[];
-}
-
-interface ToolbarItemProps {
-    key: string;
-    icon: any;
-    component: any;
-}
-
-function Toolbar({ items }: ToolbarProps) {
-    return (
-        <div className="w-full border-2 border-slate-300 rounded-md h-14 flex gap-5 items-center px-5 box-border">
-            {items.map((item) => (
-                <div className="toolbar-item" key={item.key}>
-                    {item.component}
-                </div>
-            ))}
-        </div>
-    );
-}
-
-function SwitchGroup({ isOn, handleToggle, disabled }: { isOn: boolean; handleToggle: any; disabled?: boolean }) {
-    return (
-        <>
-            <Label htmlFor="toggle-preview">Preview</Label>
-            <Switch id="toggle-preview" checked={!isOn} onCheckedChange={() => handleToggle()} disabled={disabled} />
-        </>
     );
 }
--- apps/web/app/api/ui/route.ts
+++ apps/web/app/api/ui/route.ts
@@ -1,59 +1,18 @@
-const mockData = {
-    "/": {
-        content: [
-            {
-                props: {
-                    id: "Form-18aec31e-a9e1-426c-9197-682c003bb095",
-                    inputs: [
-                        {
-                            label: "Label",
-                            type: "string",
-                            id: {
-                                value: "FFE3",
-                            },
-                        },
-                        {
-                            label: "Date",
-                            type: "date",
-                            id: {
-                                value: "AshallahMasshallah"
-                            }
-                        },
-                        {
-                            label: "Time",
-                            type: "time",
-                            id: {
-                                value: "MohammedAfizRafiz"
-                            }
-                        }
-                    ],
-                    workflowId: "",
-                },
-                type: "Form",
-            },
-            {
-                props: {
-                    id: "Form-18aec31e-a9e1-426c-9197-682c003bb094",
-                    tableId: "abcd",
-                    formName: "Form Name 123",
-                    workflowId: ""
-                },
-                type: "FormTable"
-            }
-        ],
-        root: { props: { title: "Test" } },
-        zones: {},
+const routes = [
+    {
+        route: "/",
+        id: "199299193"
     },
-    "/pricing": {
-        content: [],
-        root: { props: { title: "Pricing" } },
+    {
+        route: "/pricing",
+        id: "193810394"
     },
-    "/about": {
-        content: [],
-        root: { props: { title: "About" } },
-    },
-};
+    {
+        route: "/about",
+        id: "188388747"
+    }
+]
 
 export async function GET() {
-    return Response.json(mockData);
+    return Response.json(routes);
 }
