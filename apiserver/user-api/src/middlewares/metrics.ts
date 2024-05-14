dc2722104fead2bf6ff9bff2f6033dce269af55d
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2024-05-14 21:21:49 +0700

    feat(monitoring): separate monitoring

--- apiserver/user-api/src/middlewares/metrics.ts
+++ apiserver/user-api/src/middlewares/metrics.ts
@@ -9,14 +9,28 @@ import { MetricsService } from '../modules/metrics/metrics.service';
 export class PrometheusMiddleware implements NestMiddleware {
   httpRequests: Histogram<string>;
 
-  private logger = new Logger('PromettheusMiddleware');
+  statusCodeObserver: Histogram<string>;
+
+  responseTimeObserver: Histogram<string>;
 
   constructor(private prometheus: MetricsService) {
-    this.logger.log("Called!");
     this.httpRequests = this.prometheus.createHistogram({
       help: 'Tracks HTTP requests',
       name: 'http_requests',
-      labelNames: ['method', 'path', 'statusCode'],
+      labelNames: ['method', 'path', 'statusCode', 'responseTime'],
+      buckets: [0.1, 0.5, 1, 1.5, 2, 3, 5],
+    });
+
+    this.statusCodeObserver = this.prometheus.createHistogram({
+      help: 'Tracks HTTP status codes',
+      name: 'status_codes',
+      labelNames: ['statusCode'],
+      buckets: [0.1, 0.5, 1, 1.5, 2, 3, 5],
+    });
+
+    this.responseTimeObserver = this.prometheus.createHistogram({
+      help: 'Tracks HTTP response times',
+      name: 'response_times',
       buckets: [0.1, 0.5, 1, 1.5, 2, 3, 5],
     });
   }
@@ -27,15 +41,26 @@ export class PrometheusMiddleware implements NestMiddleware {
       const { method, route } = req;
       const { statusCode } = res;
       const duration = (Date.now() - start) / 1000;
+
       this.httpRequests.observe(
         {
           method,
           // utilizes route path instead of req.path to make sure is static (ie not affected by param values), the goal is to minimize cardinality
           path: route?.path || 'unmatched',
           statusCode,
+          responseTime: duration,
         },
         duration,
       );
+
+      this.statusCodeObserver.observe(
+        {
+          statusCode,
+        },
+        duration,
+      );
+
+      this.responseTimeObserver.observe(duration);
     });
     next();
   }
