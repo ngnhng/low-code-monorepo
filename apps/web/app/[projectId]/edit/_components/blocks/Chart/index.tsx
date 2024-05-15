7b53e40f3c27554b6154d679a3fc8691c6b37253
NgTrNghia1211 <nghia.nguyenmysterios@hcmut.edu.vn>
2024-05-15 17:33:17 +0700

    feat: integrate component chart

--- apps/web/app/[projectId]/edit/_components/blocks/Chart/index.tsx
+++ apps/web/app/[projectId]/edit/_components/blocks/Chart/index.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from "react";
-import { getClassNameFactory } from "lib";
+import cn, { getClassNameFactory } from "lib";
 
 import "chart.js/auto";
 import { ChartTypeRegistry } from "chart.js/auto";
@@ -7,7 +8,7 @@ import { Chart } from "react-chartjs-2";
 import styles from "./style.module.css";
 
 import { ComponentConfig } from "@measured/puck";
-import { Type, ChevronDown } from "react-feather";
+import { Type, ChevronDown, Table } from "react-feather";
 import Loading from "./loading";
 
 import { RowDef } from "types/table-data";
@@ -23,23 +24,20 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@repo/ui";
+import { TableSelector } from "../../table-selector";
+import { TableSelectorV2 } from "../../table-selector-v2";
+import { useLocalStorage } from "hooks/use-local-storage";
 
 const getClassNameInput = getClassNameFactory("Input", styles);
 
 export type ChartsProps = {
-  config: {
-    // UI Chart Props
-    title: string;
-    chartType: ChartType;
-    width: string;
-    height: string;
-
-    // Chart Data
-    selectedTableFields: string[];
+  title: string;
+  chartType: ChartType;
+  chartConfigs: {
     tableId: string;
-    unitX: string;
-    datasets: any[];
-    // Chart axis properties
+    selectedTableFields: any[];
+    xAxis: string;
+    yAxis: string[];
   };
 };
 
@@ -118,240 +116,88 @@ function transformDataset(rowData: RowDef[], labels: string, y: string[]) {
 
 export const Charts: ComponentConfig<ChartsProps> = {
   fields: {
-    config: {
+    title: {
+      type: "text",
+      label: "title",
+    },
+    chartType: {
+      type: "select",
+      label: "Choose chart type",
+      options: chartTypes.map((type) => ({
+        label: type,
+        value: type,
+      })),
+    },
+    chartConfigs: {
       type: "custom",
       render: ({ value, onChange }) => {
-        const {
-          projectData: { currentProjectId },
-        } = useMobxStore();
-
-        const componentsService = new ComponentsService();
-        const [checkedArray, setCheckedArray] = useState<string[]>([]);
-
-        const { data, isLoading } = useSWR(
-          "TABLE_DATA-${currentProjectId}-all",
-          () => componentsService.importTables({ projectId: currentProjectId }),
-          {
-            refreshInterval: REFRESH_INTERVAL_TIME,
-          }
-        );
-
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const Select = ({ prop, name, selectOptions }) => {
-          return (
-            <label className={getClassNameInput()}>
-              <div className={getClassNameInput("label")}>
-                <div className={getClassNameInput("labelIcon")}>
-                  <ChevronDown size={16} />
-                </div>
-                {name as string}
-              </div>
-              <select
-                className={getClassNameInput("input")}
-                onChange={(e) => {
-                  const clone = structuredClone(value);
-                  clone[prop] = e.currentTarget.value;
-
-                  onChange(clone);
-                  console.log("Clone:", clone);
-                }}
-                value={value[prop]}
-              >
-                {selectOptions.map((option) => (
-                  <option key={option} label={option} value={option} />
-                ))}
-              </select>
-            </label>
-          );
-        };
-
-        const Input = ({
-          prop,
-          name,
-          checkCallback,
-          type,
-        }: {
-          prop: string;
-          name: string;
-          type: string;
-          // eslint-disable-next-line no-unused-vars
-          checkCallback?: (value: string) => boolean;
-        }) => {
-          return (
-            <label className={getClassNameInput()}>
-              <div className={getClassNameInput("label")}>
-                <div className={getClassNameInput("labelIcon")}>
-                  <Type size={16} />
-                </div>
-                {name}
-              </div>
-              <div className="flex items-center justify-center">
-                <input
-                  type={type}
-                  className={getClassNameInput("input")}
-                  autoComplete="off"
-                  onBlur={(e) => {
-                    if (type === "text") {
-                      const clone = structuredClone(value);
-                      clone[prop] = e.currentTarget.value;
-
-                      if (
-                        !checkCallback ||
-                        checkCallback(e.currentTarget.value)
-                      )
-                        onChange(clone);
-                    }
-
-                    if (type === "checkbox") {
-                      const clone = structuredClone(value);
-                      clone[prop].push(e.currentTarget.value);
-
-                      onChange(clone);
-                      console.log(value);
-                    }
-                  }}
-                  defaultValue={type === "text" ? value[prop] : "Mock Fields"}
-                />
-                {type === "checkbox" ? <label>{name}</label> : undefined}
-              </div>
-            </label>
-          );
-        };
-
-        const handleChangeChecked = (field: string) => {
-          const isChecked = checkedArray.includes(field);
-          const clone = structuredClone(value);
-
-          if (isChecked) {
-            setCheckedArray(checkedArray.filter((f) => f !== field));
-            clone.datasets = clone.datasets.filter((f) => f !== field);
-          } else {
-            setCheckedArray([...checkedArray, field]);
-            clone.datasets.push(field);
-          }
-
-          onChange(clone);
-        };
-
         return (
-          <div>
-            <Input prop="title" name="Chart Title" type="text" />
-            <Input
-              prop="width"
-              name="Width (css units)"
-              checkCallback={cssValueCheck}
-              type="text"
-            />
-            <Input
-              prop="height"
-              name="Height (css units)"
-              checkCallback={cssValueCheck}
-              type="text"
-            />
-            <EdittedSelect
-              name="Chart Type"
-              selectOptions={chartTypes}
-              prop="chartType"
-              updateProp={onChange}
-              configProps={value}
-            />
-            {!data || isLoading ? (
-              <Loading />
-            ) : (
-              <EdittedSelect
-                prop="tableId"
-                name="Select Table"
-                selectOptions={data.data.map((table) => table.id)}
-                updateProp={onChange}
-                configProps={value}
+          <>
+            <TableSelectorV2 onChange={onChange} value={value} />
+            {value.selectedTableFields.length > 0 ? (
+              <SetAxis
+                xAxis=""
+                yAxis=""
+                chartConfigs={value}
+                selectedTableFields={value.selectedTableFields}
+                handleUpdate={onChange}
               />
-            )}
-            {value.selectedTableFields.length > 0 && (
-              <>
-                <EdittedSelect
-                  name="X Axis"
-                  prop="unitX"
-                  selectOptions={value.selectedTableFields}
-                  configProps={value}
-                  updateProp={onChange}
-                />
-                <div>
-                  <label className={getClassNameInput()}>
-                    <div className={getClassNameInput("label")}>
-                      <div className={getClassNameInput("labelIcon")}>
-                        <ChevronDown size={16} />
-                      </div>
-                      Datasets Columns
-                    </div>
-                    <ul>
-                      {value.selectedTableFields.map((field, index) => (
-                        <li
-                          key={index}
-                          className="space-x-2 flex items-center justify-start"
-                        >
-                          <Checkbox
-                            id={field}
-                            checked={checkedArray.includes(field)}
-                            onCheckedChange={() => handleChangeChecked(field)}
-                          />
-                          <label htmlFor={field}>{field}</label>
-                        </li>
-                      ))}
-                    </ul>
-                  </label>
-                </div>
-              </>
-            )}
-          </div>
+            ) : undefined}
+          </>
         );
       },
     },
   },
   defaultProps: {
-    config: {
-      title: "Sample Chart",
-      chartType: "bar",
-      width: "100%",
-      height: "600px",
+    title: "",
+    chartType: "bar",
+    chartConfigs: {
       tableId: "",
-      unitX: "",
       selectedTableFields: [],
-      datasets: [],
+      xAxis: "",
+      yAxis: [],
     },
   },
-  render: ({ config }) => {
+  render: ({ title, chartType, chartConfigs }) => {
     const {
+      tableData: { tables, fetchTableData },
       projectData: { currentProjectId },
     } = useMobxStore();
+    const [yalcToken] = useLocalStorage("yalc_at", "");
 
-    const componentsService = new ComponentsService();
+    console.log("CHART_CONFIG", chartConfigs);
 
     const { data, isLoading } = useSWR(
-      config.tableId
-        ? `TABLE_DATA-${currentProjectId}-${config.tableId}-rows`
-        : undefined,
+      chartConfigs.tableId === ""
+        ? undefined
+        : ["tables_rows", currentProjectId, chartConfigs.tableId],
       () =>
-        componentsService.importTableDataRows({
-          projectId: currentProjectId,
-          tableId: config.tableId,
-        }),
-      {
-        refreshInterval: REFRESH_INTERVAL_TIME,
-      }
+        fetchTableData(
+          {
+            tableId: chartConfigs.tableId,
+            query: {
+              sql: "(1=1)",
+              params: [],
+            },
+          },
+          yalcToken
+        )
     );
 
-    useEffect(() => {
-      if (!data) {
-        return;
-      }
-      console.log("[Data]:", data);
-      config.selectedTableFields = Object.keys(data.data[0]);
-    }, [data, config.tableId]);
+    if (!chartConfigs.tableId) {
+      return (
+        <div className="flex w-full h-96 justify-center items-center bg-slate-100 rounded-md">
+          Select a table
+        </div>
+      );
+    }
 
-    // if (!config.selectedTableFields) {}
-
-    if (config.tableId && (!data || isLoading)) {
-      return <Loading />;
+    if (!data || isLoading) {
+      return (
+        <div className="flex w-full h-96 justify-center items-center bg-slate-100 rounded-md">
+          Loading ...
+        </div>
+      );
     }
 
     return (
@@ -369,17 +215,17 @@ export const Charts: ComponentConfig<ChartsProps> = {
         <div
           style={{
             maxWidth: "1280px",
-            width: config.width,
-            height: config.height,
+            height: "450px",
+            width: "100%",
           }}
         >
           <Chart
-            type={config.chartType as keyof ChartTypeRegistry}
+            type={chartType as keyof ChartTypeRegistry}
             data={
               transformDataset(
-                data ? data.data : undefined,
-                config.unitX,
-                config.datasets
+                data ? data.rows : undefined,
+                chartConfigs.xAxis,
+                chartConfigs.yAxis
               )!
             }
             options={{
@@ -392,11 +238,101 @@ export const Charts: ComponentConfig<ChartsProps> = {
             fontSize: "1.5rem",
           }}
         >
-          {config.title}
+          {title}
         </div>
       </div>
     );
   },
+};
+
+const SetAxis = ({
+  selectedTableFields,
+  xAxis,
+  yAxis,
+  handleUpdate,
+  chartConfigs,
+}: {
+  selectedTableFields: any[];
+  xAxis: string;
+  yAxis: string;
+  handleUpdate: any;
+  chartConfigs: any;
+}) => {
+  const onChangeXAxis = (value) => {
+    handleUpdate({
+      ...chartConfigs,
+      xAxis: value,
+    });
+  };
+
+  const onChangeYAxis = (value) => {
+    handleUpdate({
+      ...chartConfigs,
+      yAxis: value,
+    });
+  };
+
+  const [yAxisValues, setYAxisvalues] = useState<string[]>([]);
+
+  return (
+    <div>
+      <div className={cn(getClassNameInput(), "mt-2")}>
+        <Label className="flex items-center justify-start mb-2">
+          <div className="mr-1">
+            <ChevronDown size={16} />
+          </div>
+          Choose x-Axis
+        </Label>
+
+        <SelectShadCn
+          onValueChange={(value) => onChangeXAxis(value)}
+          defaultValue={xAxis ?? undefined}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Select a column for x-axis" />
+          </SelectTrigger>
+          <SelectContent>
+            {selectedTableFields.map((table) => (
+              <SelectItem value={table.name} key={table.id}>
+                {table.label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </SelectShadCn>
+      </div>
+
+      <div className="mt-2">
+        <Label className="flex items-center justify-start mb-2">
+          <div className="mr-1">
+            <ChevronDown size={16} />
+          </div>
+          Choose y-Axis
+        </Label>
+
+        {selectedTableFields.map((field) => (
+          <div className="flex flex-row items-start space-x-3 space-y-0">
+            <Checkbox
+              checked={yAxisValues.includes(field.name)}
+              onCheckedChange={(checked) => {
+                if (checked) {
+                  setYAxisvalues([...yAxisValues, field.name]);
+                  onChangeYAxis([...yAxisValues, field.name]);
+                } else {
+                  setYAxisvalues(
+                    yAxisValues.filter((item) => item !== field.name)
+                  );
+                  onChangeYAxis(
+                    yAxisValues.filter((item) => item !== field.name)
+                  );
+                }
+              }}
+            />
+            <Label className="text-sm font-normal">{field.label}</Label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export const EdittedSelect = ({
