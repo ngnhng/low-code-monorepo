e14bb9abfb297ccb5b8a10081bb9516c2c4250c2
nguyendhst <nguyendhst@proton.me>
2024-05-29 12:31:15 +0700

    feat(ci): add standalone

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line unicorn/prefer-module
 module.exports = {
-  transpilePackages: ["@repo/ui"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "fukutotojido.s-ul.eu",
-      }
-    ]
-  }
+    output: "standalone",
+    transpilePackages: ["@repo/ui"],
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "fukutotojido.s-ul.eu",
+            },
+        ],
+    },
 };
